6d5d988660eb19f0bf7318a869d819b21040777a
Giselle Quarto <gisellequarto@gmail.com>
2021-05-14 12:19:01 +0100

    operators maths

---
+++ Arithmetic Operators.py
@@ -0,0 +1,14 @@
+# Write an expression that calculates the average of 23, 32 and 64
+# Place the expression in this print statement
+print((23 + 32 + 64) / 3)
+
+
+# One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long. 
+# Tiles come in packages of 6.
+# Fill this in with an expression that calculates how many tiles are needed.
+print((9 * 7) + (5 * 7))
+
+# You buy 17 packages of tiles containing 6 tiles each
+# Fill this in with an expression that calculates how many tiles will be left over.
+print((17 * 6) - (9 * 7 + 5 * 7))
+

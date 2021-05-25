6b781ad1a5d16346e5cf51966efbeb220b21899b
Giselle Quarto <gisellequarto@gmail.com>
2021-05-25 12:18:56 +0100

    exer

--- OtherLibraries/Exercise Script.py
+++ OtherLibraries/Exercise Script.py
@@ -1,9 +1,22 @@
 # Write your code here
-
 # HINT: create a dictionary from flowers.txt
+def create_flowers_dict(filename):
+    flw_dict = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            flw_list = line.replace('\n', '').split(': ')
+            flw_dict[flw_list[0]] = flw_list[1]
+    
+    return flw_dict
 
 # HINT: create a function to ask for user's first and last name
+def main():
+    flower_dict = create_flowers_dict('OtherLibraries/flowers.txt')
 
+    #Getting the first letter
+    first_letter =  input("Enter your First [space] Last name only: ").title().split(" ")[0][0]
 
-# print the desired output
+    # print the desired output
+    print('Unique flower name with the {} letter: {}'.format(first_letter, flower_dict[first_letter]))
 
+main()
--- Scripts and Errors/ReadFile.py
+++ Scripts and Errors/ReadFile.py
@@ -3,13 +3,13 @@ def create_cast_list(filename):
     #use with to open the file filename
     #use the for loop syntax to process each line
     #and add the actor name to cast_list
-    with open('flying_circus_cast.txt', 'r') as f:
+    with open(filename, 'r') as f:
         cast_list = [line.split(',')[0] for line in f]
 
     return cast_list
 
 
 
-cast_list = create_cast_list('flying_circus_cast.txt')
+cast_list = create_cast_list('Scripts and Errors/flying_circus_cast.txt')
 for actor in cast_list:
     print(actor)

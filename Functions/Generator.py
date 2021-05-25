941623eeb04d48e62a3f082a088d75bec313b5a3
Giselle Quarto <gisellequarto@gmail.com>
2021-05-25 15:10:58 +0100

    generetor

---
+++ Functions/Generator.py
@@ -0,0 +1,25 @@
+lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+def my_enumerate(iterable, start=0):
+    # Implement your generator function here
+    count = start
+    for element in iterable:
+        yield count, element
+        count += 1
+
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))
+
+
+###########################################
+
+def chunker(iterable, size):
+    # Implement function here
+     for i in range(0, len(iterable), size):
+        yield iterable[i:i + size]
+
+
+
+for chunk in chunker(range(25), 4):
+    print(list(chunk))
